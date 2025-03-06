00214bda35617d5ea21bbd4f02851116dc58a71d
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-03-06 14:57:18 +0600

    Diplayinh - html

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-const number = 31;
-const animal = "goat";
-const searchUrl = "https://www.pexels.com/search/"
+// const number = 31;
+// const animal = "goat";
+// const searchUrl = "https://www.pexels.com/search/"
+
+const message = "Welcome. Get ready to maser Vue.Js 3!"
+const date = "<strong>30th June, 2023</strong>"
+
+
 
 </script>
 
-<template>
+<!-- <template>
     <div>
         <h1 class="text-2xl text-blue-600 dark:text-sky-400" >Welcome. Get ready to master Vue.Js 3!</h1>
         <h1 class="text-xl">{{ number  }}/4 = <span>  {{ number/4 }} .</span>  </h1>
@@ -17,6 +22,15 @@ const searchUrl = "https://www.pexels.com/search/"
         <h2 class="text-xl">
             <a target="_blank" :href="`https://www.pexels.com/search/${animal.toLowerCase()}`">Search For {{ animal }}</a>
         </h2>
+    </div>
+
+</template> -->
+
+<template>
+
+    <div>
+        <h1 class="text-2xl text-white-900">{{ message }}</h1>
+        <h1 class="text-xl text-white-900">Today is <span v-html="date" class="text-blue-700"></span></h1>
     </div>
 
 </template>
